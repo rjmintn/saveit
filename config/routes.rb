4e4af0b5640a61507a30210a473da0fa46eba07e
rjmintn <rjmorawski@gmail.com>
2017-10-19 14:26:28 -0400

    little oops

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'bookmarks/show'
+
+  get 'bookmarks/new'
+
+  get 'bookmarks/edit'
+
+  get 'bookmarks/create'
+
+  get 'bookmarks/update'
+
+  get 'bookmarks/destroy'
+
   resources :topics do
     resources :bookmarks
   end
