8ae4c1f2f5398097c415fc99d9a4299d819b79c9
rjmintn <rjmorawski@gmail.com>
2017-10-23 18:06:04 -0400

    quick sync

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
     resources :bookmarks
   end
 
+# For mailgun
+  post :incoming, to: 'incoming#create'
+
+# Devise
   devise_for :users
+
   get 'welcome/index'
 
   root 'topics#index'
